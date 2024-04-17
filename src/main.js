b218e7acbdd996d26a578826004b12d09eee3169
zerol <2664276518@qq.com>
2024-04-18 00:52:27 +0800

    feat: add css handler

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import count from './js/count'
 import _ from 'lodash'
 import _es from 'lodash-es'
+import './css/variables.css'
+import cssModuleA from './css/a.module.css'
+import cssModuleB from './css/b.module.css'
+import lessModuleA from './less/a.module.less'
 
 // 获取环境变量
 const env = import.meta.env
@@ -10,3 +14,11 @@ console.log(`hello ${env.APP_NAME}`)
 console.log(count(100, 200))
 console.log(_.random(0, 10))
 console.log(_es.random(0, 10))
+
+// css 模块化
+console.log('cssModuleA ==>', cssModuleA)
+console.log('cssModuleB ==>', cssModuleB)
+console.log('lessModuleA ==>', lessModuleA)
+document.getElementById('footer').className = cssModuleB.footer
+document.getElementById('header').className = lessModuleA.header
+document.getElementById('title').className = lessModuleA.title
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
+import postcssPresetEnv from 'postcss-preset-env'
+import postcssGlobalData from '@csstools/postcss-global-data'
 
 export default defineConfig(({ command, mode }) => {
   // 开发环境是 serve，生产环境是 build，取决于 vite 的执行命令，可以通过这个参数给开发和生产分开做配置
@@ -32,5 +34,46 @@ export default defineConfig(({ command, mode }) => {
       // exclude: ['lodash-es'], // 将数组中指定的依赖不进行依赖预构建，这将会产生大量的依赖请求
     },
     envPrefix: 'APP_', // 默认值：VITE_，只有具有此前缀的环境变量才能在开发代码中使用
+    css: {
+      // 可以让浏览器显示 css 样式代码所在的文件
+      devSourcemap: true,
+      // 可以对 css 模块化行为进行默认修改
+      modules: {
+        localsConvention: 'camelCase', // 生成驼峰式命名
+        scopeBehaviour: 'local', // 默认值 local，设置模块化是全局还是区域，设置为全局的时候，就意味着关闭模块化
+        // generateScopedName: '[name]_[local]_[hash:5]', // 生成的类名的规则，规则可以到 postcss 看，一般来讲也不会手动配置这个
+        hashPrefix: 'hahaha', // 生成 hash 时会带上这个前缀去生成（默认的 hash 主要是通过类名还有其它的一些字符串生成的）
+        // globalModulePaths: ['./src/css/a.module.css'], // 不想参与到 css 模块化的路径（就变成全局的了），注意：这个地方不能识别通过 path.resolve 生成的路径格式
+        // globalModulePaths: [path.resolve(__dirname, './src/css/a.module.css').replaceAll('\\', '/')], // 替换后是可行的
+      },
+      // 可以配置 css 预处理器相关的全局参数
+      preprocessorOptions: {
+        // 要使用 less 需要安装 less 依赖包
+        // 整个配置对象都会最终给到 less 的执行参数中去（lessc），具体有哪些参数，可以到 less 官网去看
+        less: {
+          math: 'always', // 将数学结果计算出来，例如：100px / 2，输出：50px
+          globalVars: { // 定义全局变量
+            blackColor: 'black',
+          },
+        },
+        sass: {},
+      },
+      // postcss 相关配置，vite 天生支持 postcss，不需要额外安装 postcss 依赖
+      postcss: {
+        plugins: [
+          // 将数据注入到 postcss 中，这可以让后续的插件将原生的 css 变量直接解析出来
+          // 注意：这个插件一定要写在 postcssPresetEnv 插件前面，否则无效
+          //  - 它只将数据注入 PostCSS，以便其他插件可以实际使用它，它不会在 CSS 的输出中添加任何内容（所以注入晚了就没用了）
+          postcssGlobalData({
+            files: [
+              path.resolve(__dirname, './src/css/variables.css'),
+            ],
+          }),
+          // 可以解决大多数兼容问题，例如：语法降级、前缀补全
+          // 可以在 package.json 中通过 browserslist 属性配置要支持的浏览器（由 browserslist 包提供配置支持，此包已经被包含在了预设中）
+          postcssPresetEnv(),
+        ],
+      },
+    },
   }
 })
